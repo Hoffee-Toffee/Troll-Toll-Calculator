8c8297a454576967fce4ada276b994a68167c4d0
student <student@pop-os.localdomain>
2023-11-22 11:35:20 +1300

    fixed datattypes in tables

--- server/db/seeds/3_favourite-bridges.js
+++ server/db/seeds/3_favourite-bridges.js
@@ -2,55 +2,55 @@ export async function seed(knex) {
   await knex('favourite_bridges').insert([
     {
       user_id: 1,
-      bridge_id: 14,
+      bridge_id: 1,
     },
     {
       user_id: 1,
-      bridge_id: 15,
+      bridge_id: 2,
     },
     {
       user_id: 1,
-      bridge_id: 16,
+      bridge_id: 3,
     },
     {
       user_id: 1,
-      bridge_id: 17,
+      bridge_id: 4,
     },
     {
       user_id: 2,
-      bridge_id: 18,
+      bridge_id: 5,
     },
     {
       user_id: 2,
-      bridge_id: 19,
+      bridge_id: 6,
     },
     {
       user_id: 2,
-      bridge_id: 20,
+      bridge_id: 7,
     },
     {
       user_id: 2,
-      bridge_id: 21,
+      bridge_id: 8,
     },
     {
       user_id: 2,
-      bridge_id: 22,
+      bridge_id: 9,
     },
     {
       user_id: 3,
-      bridge_id: 23,
+      bridge_id: 10,
     },
     {
       user_id: 3,
-      bridge_id: 24,
+      bridge_id: 11,
     },
     {
       user_id: 3,
-      bridge_id: 25,
+      bridge_id: 12,
     },
     {
       user_id: 3,
-      bridge_id: 26,
+      bridge_id: 13,
     },
   ])
 }
