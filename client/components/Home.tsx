d48f4c3b3ef3e2490255cdb494115e4770961ae9
MingGhee Fong <mingghee@hotmail.com>
2023-11-21 17:21:09 +1300

    Restructure component and routes

---
+++ client/components/Home.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Trollololololo!!!</h1>
+      <Link to="/bridges">Link</Link>
+    </>
+  )
+}
--- client/routes.tsx
+++ client/routes.tsx
@@ -2,9 +2,13 @@ import { Route, createRoutesFromElements } from 'react-router-dom'
 
 import App from './components/App.tsx'
 import Bridges from './components/Bridges.tsx'
+import Home from './components/Home.tsx'
+import SingleBridge from './components/SingleBridge.tsx'
 
 export const routes = createRoutesFromElements(
   <Route path="/" element={<App />}>
-    <Route index element={<Bridges />} />
+    <Route index element={<Home />} />
+    <Route path="bridges" element={<Bridges />} />
+    <Route path="bridges/:id" element={<SingleBridge />} />
   </Route>
 )
