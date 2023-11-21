c21411b87fbf6da23d605c74c1d789ebe9266f7f
Keeley Owen <keeleytowen@gmail.com>
2023-11-21 16:22:09 +1300

    change getBridge function name to follow documentation.

--- server/db/bridges.ts
+++ server/db/bridges.ts
@@ -1,6 +1,6 @@
 import connection from './connection.ts'
 import { Bridge } from '../../models/bridge.ts'
 
-export async function getBridges(db = connection): Promise<Bridge[]> {
+export async function getAllBridgesDb(db = connection): Promise<Bridge[]> {
   return db('bridges').select('*')
 }
