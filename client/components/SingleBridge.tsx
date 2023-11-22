c2cd07d525644535f70d37bf9c85dd656ffbc516
anthonymartin36 <anthony.martin36@gmail.com>
2023-11-23 12:07:45 +1300

    Changes to Single Bridge - for MVP fixes

--- client/components/SingleBridge.tsx
+++ client/components/SingleBridge.tsx
@@ -1,86 +1,53 @@
-// import { Link } from 'react-router-dom'
-// import BridgesList from './BridgeList'
-// import { getSingleBridgeApi } from '../api/bridge.ts'
-// import { useQuery } from '@tanstack/react-query'
-
-// export default function SingleBridge() {
-//   const {
-//     data: bridge,
-//     error,
-//     isLoading,
-//   } = useQuery({ queryKey: ['bridges'], queryFn: getSingleBridgeApi })
-
-//   if (error) {
-//     return <p>Your bridges are gone! What a massive error</p>
-//   }
-//   if (!bridge || isLoading) {
-//     return <p>Fetching bridges from auckland...</p>
-//   }
-//   return (
-//     <>
-//       <button>
-//         <Link to={'/'}>Home</Link>
-//       </button>
-//       <button>
-//         <Link to="/bridges">Bridge List</Link>
-//       </button>
-//       <h1>This is Single Troll bridedeafdadkj!</h1>
-//       <h2>{bridge.name}</h2>
-//     </>
-//   )
-// }
 import { Bridge } from '../../models/bridge.ts'
-import { getBridgesApi } from '../api/bridge.ts'
+import { getSingleBridgeApi } from '../api/bridge.ts'
 import { useQuery } from '@tanstack/react-query'
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
-export default function SingleBridge() {
-  //Props :Bridge
+export default function SingleBridge()  { //: Promise< Bridge[]>
   const { id } = useParams()
   const {
-    data: bridges,
-    error,
+    data: bridge,
+    isError,
     isLoading,
-  } = useQuery({ queryKey: ['bridges'], queryFn: getBridgesApi })
+  } : {data : Bridge | undefined, isError : boolean, isLoading : boolean}  = useQuery({ queryKey: ['bridges', id], queryFn: ()=> getSingleBridgeApi(Number(id)) })
 
-  if (error) {
+  if (isError) {
     return <p>Your bridges are gone! What a massive error</p>
   }
-  if (!bridges || isLoading) {
+  if (!bridge || isLoading) {
     return <p>Fetching bridges from auckland...</p>
   }
-
-  const bridge = bridges.filter((br) => br.id === Number(id))
+  console.log(bridge)
 
   return (
     <>
-      <h1 id="single-bridge-title">{bridge[0].name}</h1>
+      <h1 id="single-bridge-title">{bridge.name}</h1>
 
       <div className="single-bridge-container">
         <div className="single-bridge-left-div">
-          <img src="your-image.jpg" alt="Description of your image"></img>
+          <img src="your-image.jpg" alt={bridge.name}></img>
         </div>
         <div className="right-bridge-right-div">
           <p>
-            <strong>Type:</strong> {bridge[0].name}
+            <strong>Type:</strong> {bridge.type}
           </p>
           <p>
-            <strong>Lanes:</strong> {bridge[0].lanes}
+            <strong>Lanes:</strong> {bridge.lanes || 'Unknown'}
           </p>
           <p>
-            <strong>Length:</strong> {bridge[0].length_meters}
+            <strong>Length:</strong> {bridge.lengthMeters}
           </p>
           <p>
-            <strong>Location:</strong> {bridge[0].location}
+            <strong>Location:</strong> {bridge.location}
           </p>
           <p>
-            <strong>Type:</strong> {bridge[0].type}
+            <strong>Type:</strong> {bridge.type}
           </p>
           <p>
-            <strong>Year Built:</strong> {bridge[0].year_built}
+            <strong>Year Built:</strong> {bridge.yearBuilt}
           </p>
           <p>
-            <strong>Estimated Toll Earnings:</strong> {bridge[0].toll_charge}
+            <strong>Estimated Toll Earnings:</strong> {bridge.tollCharge}
           </p>
         </div>
       </div>
--- server/db/bridges.ts
+++ server/db/bridges.ts
@@ -2,9 +2,10 @@ import connection from './connection'
 import { Bridge } from '../../models/bridge'
 
 export async function getAllBridgesDb(db = connection): Promise<Bridge[]> {
-  return db('bridges').select('*')
+  return db('bridges').select('id',' name',' location',' type',' year_built as yearBuilt',' length_meters as lengthMeters',' lanes',' added_by_user as addByUser',' toll_charge as tollCharge')
 }
 
-export async function getBridgeByIdDb(id, db = connection): Promise<Bridge[]> {
-  return db('bridges').select('*').where('id', id).first()
+export async function getBridgeByIdDb(id : number, db = connection): Promise<Bridge> {
+  return db('bridges').select('id',' name',' location',' type','  year_built as yearBuilt',' length_meters as lengthMeters',' lanes',' added_by_user as addByUser',' toll_charge as tollCharge').where('id', id).first()
 }
+
