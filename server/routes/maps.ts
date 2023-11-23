02c48af9511e1323a2b9de91747ba8daa057623a
Tristan Bulmer <63182722+Hoffee-Toffee@users.noreply.github.com>
2023-11-24 11:43:15 +1300

    Added really crappy estimate of traffic

--- server/routes/maps.ts
+++ server/routes/maps.ts
@@ -16,65 +16,73 @@ router.get('/:id', async (req, res) => {
       throw new Error('Input string is not in the correct format')
     }
   }
+  function parseDuration(duration: string) {
+    return parseInt(duration.replace('s', ''), 10)
+  }
 
   // Want to return an object, with a value between 1 and 100 with an estimate on how much money this bridge would make in comparrison to other bridges, this will be a rough guide.
   // we want the live 'business' of this, relative to itself
   // We want to calculate this, based on lanes +
 
   const bridge = await dbBridge.getBridgeByIdDb(Number(req.params.id))
+  const multiplier = 1000
 
-  console.log(bridge)
-
-  console.log(bridge.origin as string)
-  console.log(bridge.destination as string)
-
-  const origin = convertStringToLatLong(bridge.origin as string)
-  const destination = convertStringToLatLong(bridge.destination as string)
-
-  const bodyData = {
-    origin: {
-      location: {
-        latLng: {
-          latitude: origin.lat,
-          longitude: origin.long,
-        },
-      },
-    },
-    destination: {
-      location: {
-        latLng: {
-          latitude: destination.lat,
-          longitude: destination.long,
-        },
-      },
-    },
-    travelMode: 'DRIVE',
-    routingPreference: 'TRAFFIC_AWARE',
-    computeAlternativeRoutes: false,
-    routeModifiers: {
-      avoidTolls: false,
-      avoidHighways: false,
-      avoidFerries: false,
-    },
-    languageCode: 'en-US',
-    units: 'IMPERIAL',
-  }
-
-  const response = await request
-    .post('https://routes.googleapis.com/directions/v2:computeRoutes')
-    .set('Content-Type', 'application/json')
-    .set('X-Goog-Api-Key', 'AIzaSyC1cT-42Of0KFtbaYlwA3lPjtki0E9xFzM')
-    .set('X-Goog-FieldMask', 'routes.distanceMeters,routes.duration')
-    .send(bodyData)
-
-  res.send(response)
+  let estimate = 0
 
   if (bridge.lanes) {
-    // Do calculation
-  }
+    const origin = convertStringToLatLong(bridge.origin as string)
+    const destination = convertStringToLatLong(bridge.destination as string)
 
-  const returnObj = {
-    generalBusyness: bridge.busyness,
+    console.log(origin)
+    console.log(destination)
+
+    const bodyData = {
+      origin: {
+        location: {
+          latLng: {
+            latitude: origin.lat,
+            longitude: origin.long,
+          },
+        },
+      },
+      destination: {
+        location: {
+          latLng: {
+            latitude: destination.lat,
+            longitude: destination.long,
+          },
+        },
+      },
+      travelMode: 'DRIVE',
+      routingPreference: 'TRAFFIC_AWARE',
+      computeAlternativeRoutes: false,
+      routeModifiers: {
+        avoidTolls: false,
+        avoidHighways: false,
+        avoidFerries: false,
+      },
+      languageCode: 'en-US',
+      units: 'IMPERIAL',
+    }
+
+    const response = await request
+      .post('https://routes.googleapis.com/directions/v2:computeRoutes')
+      .set('Content-Type', 'application/json')
+      .set('X-Goog-Api-Key', 'AIzaSyC1cT-42Of0KFtbaYlwA3lPjtki0E9xFzM')
+      .set('X-Goog-FieldMask', 'routes.distanceMeters,routes.duration')
+      .send(bodyData)
+
+    console.log(bridge.lanes)
+    console.log(Number(bridge.lengthMeters))
+    console.log(response.body.routes[0].duration)
+
+    estimate = ((parseDuration(response.body.routes[0].duration) /
+      Number(bridge.lengthMeters)) *
+      multiplier *
+      bridge.busyness *
+      bridge.lanes) as number
+
+    res.send({ estimate: estimate, busyness: bridge.busyness })
   }
 })
 
